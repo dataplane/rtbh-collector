8e74405cfb78284989a1f290f51699731bc51514
John Kristoff <jtk@cymru.com>
2016-06-12 11:54:00 -0500

    typo

--- blackhole.sql
+++ blackhole.sql
@@ -4,6 +4,6 @@ CREATE TABLE blackhole (
     data_source TEXT NOT NULL,
     route INET NOT NULL,
     origin BIGINT NOT NULL,
-    stamp TIMESTAMP WITHOUT TIME ZONE DEFAULT date_trunc('seconds'::text, now()) NOT NULL,
+    stamp TIMESTAMP WITHOUT TIME ZONE DEFAULT date_trunc('seconds'::text, now()) NOT NULL
 );
 CREATE INDEX route_index ON blackhole (route);
