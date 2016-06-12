f4f4215dd147ab782c37d96fa4fda4f71be67668
John Kristoff <jtk@cymru.com>
2016-06-12 11:57:13 -0500

    with database push

--- decix-v4rtbh-poll.pl
+++ decix-v4rtbh-poll.pl
@@ -4,15 +4,14 @@ use strict;
 
 # $Id: $
 
-# for debugging
-use Data::Dumper;
-
+use DBD::Pg;
 use English;
 use HTML::Parser;
 use HTTP::Request::Common;
 use HTTP::Response;
 use HTTP::Status qw( :constants :is status_message );
 use LWP::UserAgent;
+use POSIX qw( strftime );
 use Readonly;
 
 $OUTPUT_AUTOFLUSH = 1;
@@ -20,11 +19,12 @@ $OUTPUT_AUTOFLUSH = 1;
 Readonly my $BASE_URL     => 'https://lg.de-cix.net/#neighbour_info';
 Readonly my $ROUTE_SERVER => 'rs1-active.de-cix.net';
 Readonly my $NEXT_HOP     => '80.81.193.66';
+Readonly my $DATA_SOURCE  => 'DE-CIX';
+Readonly my $STAMP        => strftime "%Y-%m-%d %H:%M", gmtime();
 
 my $ua = LWP::UserAgent->new;
 $ua->ssl_opts( verify_hostname => 0 );    # WARNING: allow invalid cert
 
-# TODO: get current time stamp
 my $res = $ua->request(
     POST $BASE_URL,
     [
@@ -64,8 +64,22 @@ my $content = $res->decoded_content();
 my $parser  =  HTML::Parser->new( start_h => [ \&default_start, 'self,tagname' ] );
 $parser->parse($content);
 
-#TODO: roll through $parser->{prefix} list
-print Dumper($parser);
+my $dbh = db_connect();
+my ( $sql, $sth );
+
+for my $route ( keys %{$parser->{rtbh}} ) {
+    my $origin = $parser->{rtbh}{$route};
+
+    $sql = 'INSERT INTO blackhole (route,origin,stamp,data_source) VALUES (?, ?, ?, ?)';
+
+    $sth = $dbh->prepare($sql) or die 'db preparation error: ' . DBI->errstr;
+    $sth->execute( $route, $origin, $STAMP, $DATA_SOURCE ) or die 'db execute error: ' . DBI->errstr;
+}
+
+$sth->finish or die 'db finish error: ' . DBI->errstr;
+db_disconnect($dbh);
+
+exit 0;
 
 sub default_start {
     my ( $self, $tagname ) = @_;
@@ -86,14 +100,14 @@ sub get_text {
 
         next if $line !~ m{ \A [*][>] \s ( \d{1,3} (?: [.] \d{1,3} ){3} [/] \d{1,2} ) \s+ $NEXT_HOP \s }xms; 
 
-        my $prefix = $1;
+        my $route = $1;
 
         next if $line !~ m{ \s+ 100 \s+ ( (?: \d{1,10} \s ){1,} [\?|i] ) \z }xms;
         my $path = $1;
         my @asns = split /\s/, $path;
         my $origin = $asns[-2];
 
-        $self->{rtbh}{$prefix} = $origin;
+        $self->{rtbh}{$route} = $origin;
     }
  
     return;
@@ -107,6 +121,30 @@ sub end_text {
         $self->handler( start => '' );
         $self->handler( end => '' );
     }
+
+    return;
+}
+
+sub db_connect {
+    my ($arg_ref) = @_;
+    my $db_type = $arg_ref->{type} || 'Pg';
+    my $db_host = $arg_ref->{host} || '127.0.0.1';
+    my $db_port = $arg_ref->{port} || 5432;
+    my $db_user = $arg_ref->{user} || '';
+    my $db_pass = $arg_ref->{pass} || '';
+    my $db_name = $arg_ref->{db}   || 'dataplane';
+    my $db_dsn  = "DBI:$db_type:dbname=$db_name;host=$db_host;port=$db_port";
+
+    my $dbh = DBI->connect( $db_dsn, $db_user, $db_pass )
+        || die 'db_connect error: ' . DBI->errstr;
+
+    return $dbh;
+}
+
+sub db_disconnect {
+    my $dbh = shift || return;
+
+    $dbh->disconnect or die 'db disconnect error: ' . DBI->errstr;
 
     return;
 }
